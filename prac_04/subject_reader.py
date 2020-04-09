3b4feb60ebb129667fadf3530f7f7b5f0d2b87b9
SharonReginaSutio99 <sharonregina.sutio@jcu.edu.au>
2020-04-09 18:49:56 +0800

    Develop incomplete function

--- prac_04/subject_reader.py
+++ prac_04/subject_reader.py
@@ -11,20 +11,20 @@ FILENAME = "subject_data.txt"
 def main():
     data = get_data()
     print(data)
+    for index in data:
+        print("{} is taught by {} and has {} students".format(index[0], index[1], index[2]))
 
 
 def get_data():
     """Read data from file formatted like: subject,lecturer,number of students."""
     input_file = open(FILENAME)
+    compiled_lists = []
     for line in input_file:
-        print(line)  # See what a line looks like
-        print(repr(line))  # See what a line really looks like
         line = line.strip()  # Remove the \n
         parts = line.split(',')  # Separate the data into its parts
-        print(parts)  # See what the parts look like (notice the integer is a string)
-        parts[2] = int(parts[2])  # Make the number an integer (ignore PyCharm's warning)
-        print(parts)  # See if that worked
-        print("----------")
+        parts[2] = int(parts[2]) # Make the number an integer (ignore PyCharm's warning)
+        compiled_lists.append(parts)
+    return compiled_lists
     input_file.close()
 
 
