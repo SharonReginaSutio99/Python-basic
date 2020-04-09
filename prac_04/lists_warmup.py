f9e6b883aea6b859e9b5d871b2e6cd0bd84314e8
SharonReginaSutio99 <sharonregina.sutio@jcu.edu.au>
2020-04-09 16:44:29 +0800

    Warm up regarding lists

---
+++ prac_04/lists_warmup.py
@@ -0,0 +1,20 @@
+"""
+Name: Sharon Regina Sutio
+Link: https://github.com/SharonReginaSutio99/cp1404practicals
+
+"""
+numbers = [3, 1, 4, 1, 5, 9, 2]
+numbers[0]
+numbers[-1]
+numbers[3]
+numbers[:-1]
+numbers[3:4]
+5 in numbers
+7 in numbers
+"3" in numbers
+numbers + [6, 5, 3]
+numbers[0] = "ten"
+numbers[-1] = 1
+sliced_numbers = slice(2, -1)
+print(numbers[sliced_numbers])
+print(9 in numbers)
