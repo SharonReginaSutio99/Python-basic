a686ce115a97912d240d81e066284a16acf57892
SharonReginaSutio99 <sharonregina.sutio@jcu.edu.au>
2020-04-09 22:24:51 +0800

    Check username accessibility

--- prac_04/lists_exercises.py
+++ prac_04/lists_exercises.py
@@ -5,13 +5,31 @@ Link: https://github.com/SharonReginaSutio99/cp1404practicals
 
 
 def main():
+    basic_operations()
+
+    username_checker()
+
+
+def username_checker():
+    usernames = ['jimbo', 'giltson98', 'derekf', 'WhatSup', 'NicolEye', 'swei45', 'BaseInterpreterInterface',
+                 'BaseStdIn', 'Command', 'ExecState', 'InteractiveConsole', 'InterpreterInterface', 'StartServer',
+                 'bob']
+    name = input("Username: ")
+    if name in usernames:
+        print("Access granted")
+    else:
+        print("Access denied")
+
+
+def basic_operations():
+    """practise for basic list operations"""
     numbers = []
     total = 0
     for x in range(5):
         prompt = input("Number: ")
         numbers.append(prompt)
         total += int(prompt)
-    average = total/len(numbers)
+    average = total / len(numbers)
     print("The first number is {}".format(numbers[0]))
     print("The last number is {}".format(numbers[-1]))
     print("The smallest number is {}".format(min(numbers)))
