2232f0ad7af5381db34e89cd1243a08f8758b3ad
SharonReginaSutio99 <sharonregina.sutio@jcu.edu.au>
2020-04-16 18:27:39 +0800

    Creating a lottery picker generator

---
+++ prac_04/quick_picks.py
@@ -0,0 +1,15 @@
+import random
+quick_picks = int(input("How many quick picks? "))
+MINIMUM = 1
+MAXIMUM = 45
+NUMBERS_PER_LINE = 6
+picks = []
+for number_of_picks in range(quick_picks):
+    for number in range(NUMBERS_PER_LINE):
+        random_number = random.randint(MINIMUM, MAXIMUM)
+        while random_number in picks:
+            random_number = random.randint(1, 45)
+        else:
+            picks.append(random_number)
+    print(picks)
+    picks = []
