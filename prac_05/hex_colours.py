f6e1189d1b21d789669d1f4b992697b627ab7a01
SharonReginaSutio99 <sharonregina.sutio@jcu.edu.au>
2020-04-21 20:25:36 +0800

    Convert the color name into their hex codes

--- prac_05/hex_colours.py
+++ prac_05/hex_colours.py
@@ -5,13 +5,25 @@ Name: Sharon Regina Sutio
 Link: https://github.com/SharonReginaSutio99/cp1404practicals
 """
 
-CODE_TO_NAME = {"AliceBlue": "#f0f8ff", "AntiqueWhite": "#faebd7", "aquamarine1": "#7fffd4",
-                "azure1": "#f0ffff", "beige": "#f5f5dc", "bisque1": "#ffe4c4",
-                "BlanchedAlmond": "#ffebcd", "blue1": "#0000ff", "BlueViolet": "#8a2be2",
-                "Black": "000000"}
-print(CODE_TO_NAME)
+COLOR_TO_CODE = {"AliceBlue": "#f0f8ff", "AntiqueWhite": "#faebd7", "aquamarine1": "#7fffd4",
+                 "azure1": "#f0ffff", "beige": "#f5f5dc", "bisque1": "#ffe4c4",
+                 "BlanchedAlmond": "#ffebcd", "blue1": "#0000ff", "BlueViolet": "#8a2be2",
+                 "Black": "000000"}
 
-color_name = input("Enter color name: ").upper()
-while color_name != "":
-    if color_name in CODE_TO_NAME.upper():
-        print("{} is {}".format(color_name, CODE_TO_NAME[color_name]))
+
+def print_colors():
+    max_key_length = max([len(key) for key in COLOR_TO_CODE.keys()])
+    for color, code in COLOR_TO_CODE.items():
+        print("{:{col_width}} - {}".format(color, code, col_width=max_key_length))
+
+
+def main():
+    print_colors()
+    color_to_code = {color.lower(): code for color, code in COLOR_TO_CODE.items()}
+    color_name = input("Enter color name: ").strip().lower()
+    while color_name != "":
+        print("Color code is {} for {}".format(color_to_code.get(color_name), color_name))
+        color_name = input("Enter color name: ").strip().lower()
+
+
+main()
