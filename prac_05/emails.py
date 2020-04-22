856c5c9cea816db334d81ad46ac7db1902278c5e
SharonReginaSutio99 <sharonregina.sutio@jcu.edu.au>
2020-04-22 17:57:32 +0800

    Refine code to store user name and emails

--- prac_05/emails.py
+++ prac_05/emails.py
@@ -7,20 +7,41 @@ Link: https://github.com/SharonReginaSutio99/cp1404practicals
 
 
 def get_name(email):
-    full_name = email.split("@")
-    name = full_name.split(".")
-    for i in range(len(name)):
-        name[i] = name[i].title()
-    full_name = ','.join(name)
-    print(full_name)
+    """Separate name from email"""
+    possible_name = email.split("@")[0]
+    full_name = ""
+    names = possible_name.split(".")
+    for name in names:
+        full_name += name.title() + " "
+    full_name = verify_name_input(full_name)
+    return full_name
+
+
+def verify_name_input(full_name):
+    """Filter unwanted name input"""
+    name_confirmation = input("Is your name {}? (Y / N)".format(full_name))
+    name_confirmation_input = ["", "y", "n"]
+    while name_confirmation.lower() not in name_confirmation_input:
+        print("Invalid input")
+        name_confirmation = input("Is your name {}? (Y / N)".format(full_name))
+    else:
+        if name_confirmation.lower() == "" or name_confirmation.lower() == "y":
+            full_name = full_name
+        else:
+            full_name = input("Name: ")
+    return full_name
 
 
 def main():
+    """Store user's name and emails"""
+    emails_to_names = {}
     email = input("Email: ")
-    get_name(email)
+    while email != "":
+        full_name = get_name(email)
+        emails_to_names[email] = full_name
+        email = input("Email: ")
+    for email, name in emails_to_names.items():
+        print("{} ({})".format(name, email))
 
 
 main()
-
-
-
