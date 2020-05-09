5cc990f418835935cbfd88feccf7063b9257930f
SharonReginaSutio99 <sharonregina.sutio@jcu.edu.au>
2020-05-09 17:07:41 +0800

    Square number inputted.

---
+++ prac_07/KivyDemos-master/squaring.py
@@ -0,0 +1,30 @@
+"""
+CP1404/CP5632 Practical
+Kivy GUI program to square a number
+Sharon Regina Sutio
+Link: https://github.com/SharonReginaSutio99/cp1404practicals
+"""
+
+from kivy.app import App
+from kivy.lang import Builder
+from kivy.core.window import Window
+
+__author__ = 'Lindsay Ward'
+
+
+class SquareNumberApp(App):
+    """ SquareNumberApp is a Kivy App for squaring a number """
+    def build(self):
+        """ build the Kivy app from the kv file """
+        Window.size = (300, 200)
+        self.title = "Square Number 2"
+        self.root = Builder.load_file('squaring.kv')
+        return self.root
+
+    def handle_calculate(self, value):
+        """ handle calculation (could be button press or other call), output result to label widget """
+        result = value ** 2
+        self.root.ids.output_label.text = str(result)
+
+
+SquareNumberApp().run()
