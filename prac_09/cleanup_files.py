46bd4fc3ca71e7aada5c29b6ca52e4195f421b8b
SharonReginaSutio99 <sharonregina.sutio@jcu.edu.au>
2020-05-27 18:43:37 +0800

    Capitalizing song title correctly.

--- prac_09/cleanup_files.py
+++ prac_09/cleanup_files.py
@@ -20,14 +20,14 @@ def main():
         # print("(Current working directory is: {})".format(os.getcwd()))
 
         for filename in filenames:
-
-            new_name1 = get_separate_words(filename)
-            print(new_name1)
-            #print("Renaming {} to {}".format(filename, new_name2))
+            fixed_name = get_fixed_filename(filename)
+            new_name = get_separate_words(fixed_name)
+            capitalized_name = capitalize_title(new_name)
+            print("Renaming {} to {}".format(filename, capitalized_name))
 
             full_name = os.path.join(directory_name, filename)
-            new_name = os.path.join(directory_name, new_name1)
-            # os.rename(full_name, new_name)
+            new_name = os.path.join(directory_name, capitalized_name)
+            os.rename(full_name, new_name)
 
 
 def get_separate_words(filename):
@@ -41,16 +41,32 @@ def get_separate_words(filename):
         # Check the next letter and stop before last letter
         if current_index < len(filename) - 1:
             if letter not in special_character and filename[current_index + 1] not in special_character:
-                # Add a _ to separate names if next letter is uppercase
+                # Add a _ to separate names without spaces if next letter is uppercase
                 if letter.isalnum and filename[current_index + 1].isupper():
-                    new_name += "."
+                    new_name += "_"
 
     return new_name
 
 
+def capitalize_title(new_name):
+    """Capitalize titles properly."""
+    capitalized_name = ""
+    # Go through letter one by one
+    for current_index, letter in enumerate(new_name):
+        # Check the next letter and stop before last letter
+        if current_index < len(new_name) - 1:
+            if letter.islower() and new_name[current_index - 1] == "_":
+                # Upper case correctly according to title.
+                capitalized_name += letter.upper()
+            else:
+                capitalized_name += letter
+    capitalized_name += "t"
+    return capitalized_name
+
+
 def get_fixed_filename(filename):
     """Return a 'fixed' version of filename."""
-    # new_name = filename.replace(" ", "_").replace(".TXT", ".txt")
+    new_name = filename.replace(" ", "_").replace(".TXT", ".txt")
     return new_name
 
 
